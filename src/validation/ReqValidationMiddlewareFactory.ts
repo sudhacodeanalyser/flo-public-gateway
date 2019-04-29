dfe6d9a9b43495f131fe055fdb65ffdb2395eb2c
helmutkian <helmut.rohrbacher@gmail.com>
2019-04-29 16:09:26 -0700

    Don't allow properties not defined in validators

--- src/validation/ReqValidationMiddlewareFactory.ts
+++ src/validation/ReqValidationMiddlewareFactory.ts
@@ -6,22 +6,51 @@ import * as express from 'express';
 import { injectable } from 'inversify';
 import ReqValidationError from './ReqValidationError';
 
+type RequestValidator = t.TypeC<any>;
+
+function getProps(validator: any): t.Props {
+
+  return validator.props || (validator.type ? getProps(validator.type) : {});
+}
+
 @injectable()
 class ReqValidationMiddlewareFactory {
 
-  public create(reqType: t.Type<any>): express.RequestHandler {
+  public create(reqType: RequestValidator): express.RequestHandler {
     return (req: Request, res: express.Response, next: express.NextFunction) => {
-      const result = reqType.decode(req);
-      // TODO: Figure out a better of validating (this relies on t.exact)
-      const bodyDiff = _.differenceWith(_.keys(req.body), _.keys(result.value.body), _.isEqual);
-      const validationPassed = _.isEmpty(bodyDiff);
+      // Ensure no unexpected query string, URL params, or body data is
+      // accepted if those sections do not have validators defined 
+      const unexpectedSections = _.differenceWith(
+        _.keys(_.chain(req).pick(['body', 'query', 'params']).pickBy(value => !_.isEmpty(value)).value()),
+        _.keys(reqType.props),
+        _.isEqual
+      );
 
-      if (result.isRight() && validationPassed) {
+      if (!_.isEmpty(unexpectedSections)) {
+        const message = 'Unexpected section: ' + unexpectedSections.join(', ');
+
+        return next(new ReqValidationError(message));
+      }
+
+      const unexpectedProps = _.differenceWith(
+        [..._.keys(req.query), ..._.keys(req.body)],
+        [..._.keys(getProps(reqType.props.query || {})), ..._.keys(getProps(reqType.props.body || {}))],
+        _.isEqual
+      );
+
+      if (!_.isEmpty(unexpectedProps)) {
+        const message = 'Unexpected request parameters: ' + unexpectedProps.join(', ');
+
+        return next(new ReqValidationError(message));
+      }
+
+      const result = reqType.decode(req);
+
+      if (result.isRight()) {
         next();
       } else {
-        const message = result.isLeft() ?
-          PathReporter.report(result).join(', ') :
-          `Invalid request parameters: ${bodyDiff}`;
+        const message = PathReporter.report(result).join(', ');
+
         next(new ReqValidationError(message));
       }
     };
