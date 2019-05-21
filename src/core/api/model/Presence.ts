6abf51dd29f35b72bd8cb683537ec3e7b419b2dd
helmutkian <helmut.rohrbacher@gmail.com>
2019-05-20 17:42:53 -0700

    Add Kafka dependency

--- src/core/api/model/Presence.ts
+++ src/core/api/model/Presence.ts
@@ -1,16 +1,26 @@
 import * as t from 'io-ts';
 import {Expandable, TimestampedModel, Location, DeviceUpdate, DeviceType, DeviceModelType} from '../../api';
 
+const PresenceRequestCodec = t.type({
+    appName: t.string,
+    appVersion: t.union([t.string, t.undefined]),
+    ttl: t.number
+});
 
-export interface PresenceData {
-    action: string,
-    userId: string,
-    accountId: string,
-    deviceId: string[],
-    appName: string,
-    appVersion: string,
-    type: string,
-    date: string,
-    ipAddress: string,
-    ttl: number
-}
+const PresenceDataCodec = t.intersection([
+    PresenceRequestCodec,
+    t.type({
+        action: t.string,
+        userId: t.string,
+        accountId: t.union([t.string, t.undefined]),
+        deviceId: t.union([t.array(t.string), t.undefined]),
+        type: t.string,
+        date: t.string,
+        ipAddress: t.string
+    })
+]);
+
+export const PresenceRequestValidator = t.exact(t.partial(PresenceRequestCodec.props));
+
+export type PresenceRequest = t.TypeOf<typeof PresenceRequestValidator>;
+export type PresenceData = t.TypeOf<typeof PresenceDataCodec>;
--- src/core/presence/PresenceControllerFactory.ts
+++ src/core/presence/PresenceControllerFactory.ts
@@ -1,12 +1,17 @@
-import {interfaces, controller, httpGet, httpPost, requestBody, httpDelete} from 'inversify-express-utils';
+import {interfaces, controller, httpGet, httpPost, requestBody, httpDelete, request } from 'inversify-express-utils';
 import { inject, Container } from 'inversify';
 import PresenceService from './PresenceService';
 import { httpController } from '../api/controllerUtils';
 import AuthMiddlewareFactory from "../../auth/AuthMiddlewareFactory";
 import Request from "../api/Request";
-import {PresenceData} from "../api/model/Presence";
+import { PresenceData, PresenceRequestValidator } from "../api/model/Presence";
+import ReqValidationMiddlewareFactory from '../../validation/ReqValidationMiddlewareFactory';
+import * as t from 'io-ts';
+import _ from 'lodash';
+
 
 export function PresenceControllerFactory(container: Container, apiVersion: number): interfaces.Controller {
+  const reqValidator = container.get<ReqValidationMiddlewareFactory>('ReqValidationMiddlewareFactory');
   const authMiddlewareFactory = container.get<AuthMiddlewareFactory>('AuthMiddlewareFactory');
   const authWithUserId = authMiddlewareFactory.create(async ({ params: { id } }: Request) => ({ icd_id: id }));
 
@@ -16,15 +21,27 @@ export function PresenceControllerFactory(container: Container, apiVersion: numb
       @inject('PresenceService') private presenceService: PresenceService
     ) {}
 
-    @httpPost('/', authWithUserId)
-    private async report(@requestBody() presencePostRequest: PresenceData): Promise<{ [key: string]: any }> {
+    @httpPost('/', 
+      authWithUserId,
+      reqValidator.create(t.type({
+        body: PresenceRequestValidator
+      }))
+    )
+    private async report(@request() req: Request, @requestBody() presencePostRequest: PresenceData): Promise<{ [key: string]: any }> {
+      const tokenMetadata = req.token;
+
+      if (tokenMetadata === undefined) {
+        throw new Error('No token defined.');
+      }
 
       // TODO: Fill in with real data based on auth token
-      const ipAddress = "127.0.0.1";
-      const userId = "01234567";
-      const appName = "mySuperAwesomeApp";
+      const xForwardedForHeader = req.headers && req.headers['x-forwarded-for'];
+      const xForwardedFor = xForwardedForHeader === undefined ? '' : (_.isArray(xForwardedForHeader) ? xForwardedForHeader[0] : xForwardedForHeader).split(',')[0];
+      const ipAddress = xForwardedFor || (req.connection && req.connection.remoteAddress) || '';
+      const userId = tokenMetadata.user_id;
+      const clientId = tokenMetadata.client_id || 'legacy';
 
-      return this.presenceService.report(presencePostRequest, ipAddress, userId, appName);
+      return this.presenceService.report(presencePostRequest, ipAddress, userId, clientId);
     }
   }
 
