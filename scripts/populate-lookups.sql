4547823b4bff67e60ab2be600bb1b8a24d5567df
helmutkian <helmut.rohrbacher@gmail.com>
2019-06-18 15:44:53 -0700

    Bootstrap script for look ups

---
+++ scripts/populate-lookups.sql
@@ -0,0 +1,303 @@
+-- Device Makes
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('device_make_list', 'flo_device_v2', 'Flo Device', 'Flo Device', 1, 0)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('device_make_list', 'puck_oem', 'Puck', 'Puck', 1, 1)
+
+-- FbM models
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('device_model_flo_device_v2', 'flo_device_075_v2', '3/4"', '3/4" Flo Device', 1, 0)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('device_model_flo_device_v2', 'flo_device_125_v2', '1 1/4"', '1 1/4" Flo Device', 1, 1)
+
+-- Puck models
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('device_model_puck_oem', 'puck_v1', 'Puck', 'Puck', 1, 0)
+
+-- Countries
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('country', 'us', 'USA', 'United States of America', 1, 0)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('country', 'ca', 'Canada', 'Canada', 1, 1)
+
+-- US States
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'al', 'AL', 'Alabama', 1, 0)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ak', 'AK', 'Alaska', 1, 1)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'az', 'AZ', 'Arizona', 1, 2)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ar', 'AR', 'Arkansas', 1, 3)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ca', 'CA', 'California', 1, 4)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'co', 'CO', 'Colorado', 1, 5)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ct', 'CT', 'Connecticut', 1, 6)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'de', 'DE', 'Delaware', 1, 7)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'fl', 'FL', 'Florida', 1, 8)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ga', 'GA', 'Georgia', 1, 9)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'hi', 'HI', 'Hawaii', 1, 10)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'id', 'ID', 'Idaho', 1, 11)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'il', 'IL', 'Illinois', 1, 12)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'in', 'IN', 'Indiana', 1, 13)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ia', 'IA', 'Iowa', 1, 14)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ks', 'KS', 'Kansas', 1, 15)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ky', 'KY', 'Kentucky', 1, 16)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'la', 'LA', 'Louisiana', 1, 17)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'me', 'ME', 'Maine', 1, 18)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'md', 'MD', 'Maryland', 1, 19)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ma', 'MA', 'Massachusetts', 1, 20)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'mi', 'MI', 'Michigan', 1, 21)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'mn', 'MN', 'Minnesota', 1, 22)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ms', 'MS', 'Mississippi', 1, 23)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'mo', 'MO', 'Missouri', 1, 24)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'mt', 'MT', 'Montana', 1, 25)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ne', 'NE', 'Nebraska', 1, 26)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'nv', 'NV', 'Nevada', 1, 27)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'nh', 'NH', 'New Hampshire', 1, 28)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'nj', 'NJ', 'New Jersey', 1, 29)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'nm', 'NM', 'New Mexico', 1, 30)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ny', 'NY', 'New York', 1, 31)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'nc', 'NC', 'North Carolina', 1, 32)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'nd', 'ND', 'North Dakota', 1, 33)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'oh', 'OH', 'Ohio', 1, 34)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ok', 'OK', 'Oklahoma', 1, 35)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'or', 'OR', 'Oregon', 1, 36)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'pa', 'PA', 'Pennsylvania', 1, 37)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ri', 'RI', 'Rhode Island', 1, 38)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'sc', 'SC', 'South Carolina', 1, 39)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'sd', 'SD', 'South Dakota', 1, 40)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'tn', 'TN', 'Tennessee', 1, 41)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'tx', 'TX', 'Texas', 1, 42)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'ut', 'UT', 'Utah', 1, 43)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'vt', 'VT', 'Vermont', 1, 44)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'va', 'VA', 'Virginia', 1, 45)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'wa', 'WA', 'Washington', 1, 46)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'dc', 'DC', 'Washington DC', 1, 47)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'wv', 'WV', 'West Virginia', 1, 48)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'wi', 'WI', 'Wisconsin', 1, 49)
+
+INSERT INTO lookup 
+(lookup_id, key_id, short_display, long_display, "state", "order")
+VALUES 
+('us_state', 'wy', 'WY', 'Wyoming', 1, 50)
