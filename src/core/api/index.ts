c2b73ddef7beca790420e4d95d8e6a4acdf349a1
Gabriel Raineri <graineri@gmail.com>
2019-06-27 02:07:11 -0300

    Fix export order

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,18 +1,20 @@
 // Mixin Interfaces
 export * from './Expandable';
+export * from './Timestamped';
+export * from './SubscriptionProvider';
+
 // Models
 export * from './model/Account';
-export * from './model/Device';
+export * from './model/User';
 export * from './model/Location';
+export * from './model/Device';
+export * from './model/Subscription';
 export * from './model/Lookup';
 export * from './model/OnboardingLog';
-export * from './model/Subscription';
-export * from './model/User';
-export * from './SubscriptionProvider';
-export * from './Timestamped';
 
 // Utils
 export type DependencyFactoryFactory = <T>(dep: string) => () => T;
 
 export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
