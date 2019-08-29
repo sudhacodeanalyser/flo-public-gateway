decbf9fbfee63f06de9b8d98838c2b40644c9186
Artem Zhirkov <az@develdynamic.com>
2019-08-29 20:13:54 +0000

    Revert "Merge branch 'test-ingress-split' into 'dev'"
    This reverts merge request !375

--- k8s/env-prod.sh
+++ k8s/env-prod.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
 ENVIRONMENT=$1
 
-envsubst < k8s/pipeline-prod.yaml.tpl > k8s/pipeline.yaml
-
+sed -i  "s/  tag:.*/  tag: '$CI_PIPELINE_ID'/g" k8s/pipeline.yaml
+sed -i  "s/    application_name:.*/    application_name: $APPLICATION_NAME_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    docs_endpoint_password:.*/    docs_endpoint_password: $DOCS_ENDPOINT_PASSWORD_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    docs_endpoint_user:.*/    docs_endpoint_user: $DOCS_ENDPOINT_USER_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    environment:.*/    environment: production/g" k8s/pipeline.yaml
+sed -i  "s/    external_docs_endpoint_password:.*/    external_docs_endpoint_password: $EXTERNAL_DOCS_ENDPOINT_PASSWORD_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    external_docs_endpoint_user:.*/    external_docs_endpoint_user: $EXTERNAL_DOCS_ENDPOINT_USER_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    internal_device_service_base_url:.*/    internal_device_service_base_url: $INTERNAL_DEVICE_SERVICE_BASE_URL_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    kafka_host:.*/    kafka_host: $KAFKA_HOST_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    kafka_timeout_ms:.*/    kafka_timeout_ms: '$KAFKA_TIMEOUT_MS_PROD'/g" k8s/pipeline.yaml
+sed -i  "s/    psotgres_database:.*/    psotgres_database: $POSTGRES_DATABASE_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    postgres_host:.*/    postgres_host: $POSTGRES_HOST_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    postgres_password:.*/    postgres_password: $POSTGRES_PASSWORD_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    postgres_port:.*/    postgres_port: '$POSTGRES_PORT_PROD'/g" k8s/pipeline.yaml
+sed -i  "s/    postgres_user:.*/    postgres_user: $POSTGRES_USER_PROD/g" k8s/pipeline.yaml
+sed -i  "s/    localization_url:.*/    localization_url: $LOCALIZATION_API_URL_PROD/g" k8s/pipeline.yaml
