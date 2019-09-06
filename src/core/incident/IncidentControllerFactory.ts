c63802034f9f123deb25fdabac35fc873ae27b42
Alex Zherdev <alex@flotechnologies.com>
2019-09-06 14:43:52 -0700

    Added incident gets

--- src/core/incident/IncidentControllerFactory.ts
+++ src/core/incident/IncidentControllerFactory.ts
@@ -1,13 +1,21 @@
+import * as Either from 'fp-ts/lib/Either';
+import { pipe } from 'fp-ts/lib/pipeable';
 import { Container, inject } from 'inversify';
 import { BaseHttpController, httpDelete, httpGet, httpPost, interfaces, request, requestBody, requestParam } from 'inversify-express-utils';
+import * as t from 'io-ts';
+import _ from 'lodash';
 import AuthMiddlewareFactory from '../../auth/AuthMiddlewareFactory';
+import ReqValidationError from '../../validation/ReqValidationError';
 import ReqValidationMiddlewareFactory from '../../validation/ReqValidationMiddlewareFactory';
+import { AlarmEvent, AlertFeedback, NotificationStatistics, PaginatedResult, UserFeedback, UserFeedbackCodec } from '../api';
 import { createMethod, httpController } from '../api/controllerUtils';
 import Request from '../api/Request';
 import { NotificationServiceFactory } from '../notification/NotificationService';
 import { AlertService } from '../service';
 
+
 export function IncidentControllerFactory(container: Container, apiVersion: number): interfaces.Controller {
+  const reqValidator = container.get<ReqValidationMiddlewareFactory>('ReqValidationMiddlewareFactory');
   const authMiddlewareFactory = container.get<AuthMiddlewareFactory>('AuthMiddlewareFactory');
   const auth = authMiddlewareFactory.create();
 
@@ -28,6 +36,27 @@ export function IncidentControllerFactory(container: Container, apiVersion: numb
         .sendAlarm(incidentInfo);
     }
 
+    @httpGet('/')
+    private async getIncidentByFilter(@request() req: Request): Promise<PaginatedResult<AlarmEvent>> {
+      const filters = req.url.split('?')[1] || '';
+
+      if (_.isEmpty(req.query.deviceId) && _.isEmpty(req.query.locationId)) {
+        throw new ReqValidationError('Missing deviceId or locationId parameters.');
+      }
+
+      return this.eventService.getAlarmEventsByFilter(filters);
+    }
+
+    @httpGet('/:id',
+        reqValidator.create(t.type({
+          params: t.type({
+            id: t.string
+          })
+        }))
+    )
+    private async getIncident(@requestParam('id') id: string): Promise<AlarmEvent> {
+      return this.eventService.getAlarmEvent(id);
+    }
   }
 
   return EventController;
