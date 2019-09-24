b72df20cc76c30b2953b1bd6764ee77f34d11c17
Facundo Rossi <frossi85@gmail.com>
2019-09-23 23:01:05 -0300

    Remove hacky code for small drips sensitivity and flo sense alarm setting, now all comes from notification api

--- src/core/notification/containerModule.ts
+++ src/core/notification/containerModule.ts
@@ -5,6 +5,5 @@ import { NotificationServiceFactory } from "./NotificationService";
 
 export default new ContainerModule((bind: interfaces.Bind) => {
   bind<string>('notificationApiUrl').toConstantValue(config.notificationApiUrl);
-  bind<number>('defaultFloSenseLevel').toConstantValue(config.defaultFloSenseLevel);
   bind<NotificationServiceFactory>('NotificationServiceFactory').to(ApiNotificationServiceFactory);
 });
