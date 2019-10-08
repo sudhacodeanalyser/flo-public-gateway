fa81539499863a5e6abd81bb458caf856efbf8e7
Gabriel Raineri <graineri@gmail.com>
2019-10-08 18:21:45 -0300

    Adapt current POST /devices/:id/telemetry to Puck
    * Allow Puck telemetry in endpoint
    * Publish telemetry to corresponding puck topic in Kafka

--- src/core/telemetry/TelemetryService.ts
+++ src/core/telemetry/TelemetryService.ts
@@ -1,25 +1,31 @@
-import { injectable, inject } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { KafkaProducer } from '../../kafka/KafkaProducer';
 import { Telemetry } from '../api';
 
-@injectable() 
+@injectable()
 class TelemetryService {
 
   constructor(
     @inject('TelemetryKafkaTopic') private readonly telemetryKafkaTopic: string,
+    @inject('PuckTelemetryKafkaTopic') private readonly puckTelemetryKafkaTopic: string,
     @inject('KafkaProducer') private readonly kafkaProducer: KafkaProducer,
   ) {}
 
   public async publishTelemetry(telemetryMessages: Telemetry[]): Promise<void> {
-    const promises = telemetryMessages.map(telemetryMessage => 
+    const promises = telemetryMessages.map(telemetryMessage =>
       this.kafkaProducer.send(
-        this.telemetryKafkaTopic, 
+        this.telemetryKafkaTopic,
         telemetryMessage
       )
     );
 
     await Promise.all(promises);
   }
+
+  public async publishPuckTelemetry(telemetry: any): Promise<void> {
+    return this.kafkaProducer.send(this.puckTelemetryKafkaTopic, telemetry);
+  }
 }
 
-export { TelemetryService }
+export { TelemetryService };
+
