02f46f6f6fade1b6731a3d1603ef105f2b3fb877
Gabriel Raineri <graineri@gmail.com>
2020-02-12 20:38:32 -0300

    Alert Feedback Flow - NR API

--- src/core/alarm/AlarmService.ts
+++ src/core/alarm/AlarmService.ts
@@ -2,9 +2,7 @@ import { injectable, inject } from 'inversify';
 import { injectHttpContext, interfaces } from 'inversify-express-utils';
 import { NotificationServiceFactory, NotificationService } from '../notification/NotificationService';
 import _ from 'lodash';
-import AlertFeedbackFlowTable from './AlertFeedbackFlowTable';
-import { AlarmListResult, Alarm, DeviceSystemModeNumeric } from '../api';
-import { AlertFeedbackFlowRecord, AlertFeedbackFlowRecordData } from './AlertFeedbackFlowRecord';
+import { AlarmListResult, Alarm } from '../api';
 
 @injectable()
 class AlarmService {
@@ -12,7 +10,6 @@ class AlarmService {
 
   constructor(
     @inject('NotificationServiceFactory') notificationServiceFactory: NotificationServiceFactory,
-    @inject('AlertFeedbackFlowTable') private alertFeedbackFlowTable: AlertFeedbackFlowTable,
     @injectHttpContext private readonly httpContext: interfaces.HttpContext
   ) {
 
@@ -25,37 +22,12 @@ class AlarmService {
     };
   }
 
-  public async getAlarms(filters: string): Promise<AlarmListResult> {
-    const alarms = await this.notificationServiceFactory().getAlarms(filters);
-    const alarmsWithFeedbackFlow = await Promise.all(
-      alarms.items
-        .map(async alarm => this.joinAlarmWithFeedbackFlow(alarm))
-    );
-
-    return {
-      ...alarms,
-      items: alarmsWithFeedbackFlow
-    }
+  public async getAlarms(queryParams: any): Promise<AlarmListResult> {
+    return this.notificationServiceFactory().getAlarms(queryParams);
   }
 
-  public async getAlarmById(id: string): Promise<Alarm> {
-    const alarm = await this.notificationServiceFactory().getAlarmById(id);
-
-    return this.joinAlarmWithFeedbackFlow(alarm);
-  }
-
-  private async joinAlarmWithFeedbackFlow(alarm: Alarm): Promise<Alarm> {
-    const alertFeedbackFlowRecords = await Promise.all([
-      this.alertFeedbackFlowTable.get({ alarm_id: alarm.id, system_mode: DeviceSystemModeNumeric.HOME }),
-      this.alertFeedbackFlowTable.get({ alarm_id: alarm.id, system_mode: DeviceSystemModeNumeric.AWAY })
-    ]);
-
-    return {
-      ...alarm,
-      userFeedbackFlow: alertFeedbackFlowRecords
-        .filter(alertFeedbackFlow => alertFeedbackFlow !== null && !_.isEmpty(alertFeedbackFlow))
-        .map(alertFeedbackFlow => AlertFeedbackFlowRecord.toModel(alertFeedbackFlow as AlertFeedbackFlowRecordData))
-    };
+  public async getAlarmById(id: string, queryParams: any): Promise<Alarm> {
+    return this.notificationServiceFactory().getAlarmById(id, queryParams);
   }
 }
 
