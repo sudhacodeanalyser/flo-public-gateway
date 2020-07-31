10c18f174ef0983822d7c54f0e0bf1a59a1053bd
Gonzalo Denari <gonzalo@flotechnologies.com>
2020-07-31 17:07:54 -0300

    return array of invalid - part 2

--- src/validation/ReqValidationError.ts
+++ src/validation/ReqValidationError.ts
@@ -1,7 +1,7 @@
 import ExtendableError from '../core/api/error/ExtendableError';
 
 export default class ReqValidationError extends ExtendableError {
-  constructor(message: string, invalidFields?: any[]) {
-    super(message, 400, invalidFields && { invalidFields });
+  constructor(message: string, invalidProperties?: any[]) {
+    super(message, 400, invalidProperties && { invalidProperties });
   }
 }
