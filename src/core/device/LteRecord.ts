df1790fe5df4f7804348f8f4626172d98cae0ea2
Gabriel Raineri <graineri@gmail.com>
2020-09-23 17:31:19 -0300

    Add /devices/lte endpoint

---
+++ src/core/device/LteRecord.ts
@@ -0,0 +1,30 @@
+import { morphism, StrictSchema } from 'morphism';
+import { Lte } from '../api';
+
+export interface LteRecordData {
+  imei: string;
+  icc_id: string;
+  rnd_key: string;
+  qr_code: string;
+  ssid_offset: number;
+  pwd_offset: number;
+}
+
+const RecordToModelSchema: StrictSchema<Lte, LteRecordData> = {
+  imei: 'imei',
+  iccId: 'icc_id',
+  randomKey: 'rnd_key',
+  qrCode: 'qr_code',
+  ssidOffset: 'ssid_offset',
+  passwordOffset: 'pwd_offset'
+}
+
+export class LteRecord {
+  constructor(
+    public data: LteRecordData
+  ) {}
+
+  public toModel(): Lte {
+    return morphism(RecordToModelSchema, this.data);
+  }
+}
