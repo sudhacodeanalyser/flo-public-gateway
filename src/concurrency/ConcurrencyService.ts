46b51914e3e1ee57107212bff6337f31ba92806e
Gabriel Raineri <graineri@gmail.com>
2020-10-08 16:43:06 -0300

    Add lock in /pairing/complete endpoint

---
+++ src/concurrency/ConcurrencyService.ts
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+import { inject, injectable } from 'inversify';
+import Redis from 'ioredis';
+
+@injectable()
+export default class ConcurrencyService {
+  
+  constructor(
+    @inject('RedisClient') private redisClient: Redis.Redis
+  ) {}
+
+  public async acquireLock(key: string, ttlSecs: number): Promise<boolean> {
+    // Lock algorithm described here: https://redis.io/commands/set
+    const hasLock = await this.redisClient.set(key, new Date().getTime() + ttlSecs  * 1000, 'EX', ttlSecs, 'NX');
+    return !_.isNil(hasLock);
+  }
+
+  public async releaseLock(key: string): Promise<void> {
+    await this.redisClient.del(key);
+  }
+}
