ff47fd3cccd262629905dc7e490e5a8895c26a20
Huy Nguyen <huy@flotechnologies.com>
2020-12-23 11:01:57 -0800

    force null defaults on ts

--- scripts/sql/email_change.sql
+++ scripts/sql/email_change.sql
@@ -6,10 +6,10 @@ create table email_change (
     user_id uuid not null,
     old_email varchar(256) not null,
     old_conf_key uuid not null,
-    old_conf_on timestamp without time zone,
+    old_conf_on timestamp without time zone default null,
     new_email varchar(256) not null,
     new_conf_key uuid not null,
-    new_conf_on timestamp without time zone,
+    new_conf_on timestamp without time zone default null,
     created timestamp without time zone default current_timestamp not null
 );
 create index on email_change (user_id);
