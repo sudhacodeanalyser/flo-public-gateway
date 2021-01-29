71c6fa14fbdf38bb47c82fca77aa1dec6a6f2986
Gabriel Raineri <graineri@gmail.com>
2021-01-29 17:31:06 -0300

    Configure callback gateway host

--- src/core/delivery-hook/TwilioAuthMiddlewareFactory.ts
+++ src/core/delivery-hook/TwilioAuthMiddlewareFactory.ts
@@ -9,10 +9,10 @@ class TwilioAuthMiddlewareFactory {
   @inject('TwilioAuthToken') private twilioAuthToken: string;
   @inject('PublicGatewayHost') private host: string;
 
-  public create(): express.Handler {
+  public create(originHost?: string): express.Handler {
     return async (req: Request, res: express.Response, next: express.NextFunction): Promise<void> => {
       try {
-        const url = 'https://' + (this.host || req.get('host')) + req.originalUrl;
+        const url = 'https://' + (originHost || this.host || req.get('host')) + req.originalUrl;
         const twilioSignature = req.get('x-twilio-signature') || '';
 
         if (client.validateRequest(this.twilioAuthToken, twilioSignature, url, req.body)) {
--- src/core/delivery-hook/containerModule.ts
+++ src/core/delivery-hook/containerModule.ts
@@ -5,5 +5,6 @@ import config from "../../config/config";
 export default new ContainerModule((bind: interfaces.Bind) => {
   bind<string>('TwilioAuthToken').toConstantValue(config.twilioAuthToken);
   bind<string>('PublicGatewayHost').toConstantValue(config.publicGatewayHost);
+  bind<string>('CallbackGatewayHost').toConstantValue(config.callbackGatewayHost);
   bind<TwilioAuthMiddlewareFactory>('TwilioAuthMiddlewareFactory').to(TwilioAuthMiddlewareFactory);
 });
