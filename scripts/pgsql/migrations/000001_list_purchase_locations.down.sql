dbfeb3053a355c4590223cc0116ba1bd40cd7e39
Daniel Rodriguez <daniel.rodriguez@flotechnologies.com>
2021-04-26 13:20:45 -0300

    [cloud-4443] down step

--- scripts/pgsql/migrations/000001_list_purchase_locations.down.sql
+++ scripts/pgsql/migrations/000001_list_purchase_locations.down.sql
@@ -1,3 +1,3 @@
 delete from list 
-where list_id = 'purchase_location' and key_id in ('meetflo', 'amazon')
-and lang in ('en')
+where list_id = 'purchase_location' and key_id in ('meetflo', 'amazon', 'plumber', 'lennar','other', 'ns','homeimprovementstore')
+and lang in ('en', 'es')
