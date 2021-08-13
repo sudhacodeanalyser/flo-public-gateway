c36d5bb1248d937e04fdbe71c7c1485431f14c80
Martin Ting <imnotmartin@gmail.com>
2021-08-13 10:26:56 -0700

    allow admin to bypass account id validation for invite resend

--- src/core/account/AccountControllerFactory.ts
+++ src/core/account/AccountControllerFactory.ts
@@ -78,7 +78,7 @@ export function AccountControllerFactory(container: Container, apiVersion: numbe
     @httpPost('/invite/resend',
       authWithIdBody,
       reqValidator.create(t.type({
-        body: t.type({
+        body: t.partial({
           accountId: accountIdValidator,
           email: emailValidator,
         })
